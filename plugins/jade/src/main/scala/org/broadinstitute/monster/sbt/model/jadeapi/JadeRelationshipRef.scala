4b2c843c41ec11a185976c0f53647955ae4f7586
Dan Moran <danmoran@broadinstitute.org>
2020-01-31 16:07:38 -0500

    Remove cardinality injection.

--- plugins/jade/src/main/scala/org/broadinstitute/monster/sbt/model/jadeapi/JadeRelationshipRef.scala
+++ plugins/jade/src/main/scala/org/broadinstitute/monster/sbt/model/jadeapi/JadeRelationshipRef.scala
@@ -1,6 +1,6 @@
 package org.broadinstitute.monster.sbt.model.jadeapi
 
-import io.circe.{Encoder, Json}
+import io.circe.Encoder
 import io.circe.derivation.deriveEncoder
 import org.broadinstitute.monster.sbt.model.JadeIdentifier
 
@@ -10,14 +10,5 @@ case class JadeRelationshipRef(
 )
 
 object JadeRelationshipRef {
-  /**
-    * Constant cardinality to use for all Jade relationships.
-    *
-    * Jade validates that this string matches an enum list but doesn't use the
-    * value for anything else, so we don't bother specifying / deriving it.
-    */
-  private val cardinality = Json.fromString("many")
-
-  implicit val encoder: Encoder[JadeRelationshipRef] =
-    deriveEncoder.mapJsonObject(_.add("cardinality", cardinality))
+  implicit val encoder: Encoder[JadeRelationshipRef] = deriveEncoder
 }
