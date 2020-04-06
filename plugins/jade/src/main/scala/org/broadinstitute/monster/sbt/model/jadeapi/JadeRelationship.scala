30c4224950978cf84a683523415f0a2e50436a46
Daniel Moran <danmoran@broadinstitute.org>
2020-04-06 16:28:12 -0400

    DSPDC-962 Stop using random UUIDs for relationship names (#21)

--- plugins/jade/src/main/scala/org/broadinstitute/monster/sbt/model/jadeapi/JadeRelationship.scala
+++ plugins/jade/src/main/scala/org/broadinstitute/monster/sbt/model/jadeapi/JadeRelationship.scala
@@ -1,21 +1,22 @@
 package org.broadinstitute.monster.sbt.model.jadeapi
 
-import java.util.UUID
-
-import io.circe.{Encoder, Json}
+import io.circe.Encoder
 import io.circe.derivation.deriveEncoder
 
 case class JadeRelationship(
+  name: String,
   from: JadeRelationshipRef,
   to: JadeRelationshipRef
 )
 
 object JadeRelationship {
 
-  // Jade requires that relationships have names, so we generate
-  // fake ones. THIS IS NOT DETERMINISTIC.
-  implicit val encoder: Encoder[JadeRelationship] =
-    deriveEncoder.mapJsonObject { base =>
-      base.add("name", Json.fromString(UUID.randomUUID().toString))
-    }
+  def apply(from: JadeRelationshipRef, to: JadeRelationshipRef): JadeRelationship =
+    JadeRelationship(
+      name = s"from_${from.table.id}.${from.column.id}_to_${to.table.id}.${to.column.id}",
+      from = from,
+      to = to
+    )
+
+  implicit val encoder: Encoder[JadeRelationship] = deriveEncoder
 }
