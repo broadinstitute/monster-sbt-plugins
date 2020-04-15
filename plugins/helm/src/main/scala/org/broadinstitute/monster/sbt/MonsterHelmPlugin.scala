193d197f3b88ceee8db80439acd6ab959c6ffc04
Dan Moran <danmoran@broadinstitute.org>
2020-04-15 14:36:35 -0400

    DSPDC-987 Separate Helm upload from index.

--- plugins/helm/src/main/scala/org/broadinstitute/monster/sbt/MonsterHelmPlugin.scala
+++ plugins/helm/src/main/scala/org/broadinstitute/monster/sbt/MonsterHelmPlugin.scala
@@ -4,6 +4,7 @@ import io.circe.{Json, yaml}
 import io.circe.yaml.syntax._
 import sbt._
 import sbt.Keys._
+import sbt.nio.Keys._
 import scala.sys.process._
 import scala.util.{Failure, Success, Try}
 
@@ -23,71 +24,92 @@ object MonsterHelmPlugin extends AutoPlugin {
 
   import autoImport._
 
+  def packageChart(
+    chartRoot: File,
+    version: String,
+    targetDir: File
+  ): Unit = {
+    val tmpDir = IO.createTemporaryDirectory
+    IO.copyDirectory(chartRoot, tmpDir)
+    // Wipe out the target/ directory, if it was copied over.
+    IO.delete(tmpDir / "target")
+
+    // Inject the version and appVersion so packaging does the right thing.
+    val chartMetadata = chartRoot / "Chart.yaml"
+    val parsedMetadata = yaml.parser.parse(IO.read(chartMetadata)) match {
+      case Right(parsed) => parsed
+      case Left(err)     => sys.error(s"Could not parse $chartMetadata as YAML: ${err.getMessage}")
+    }
+    val realVersion = Json.fromString(version)
+    val updatedMetadata =
+      parsedMetadata.deepMerge(Json.obj("version" -> realVersion, "appVersion" -> realVersion))
+    IO.write(tmpDir / "Chart.yaml", updatedMetadata.asYaml.spaces2)
+
+    // Use helm to package the staged template.
+    // Assumes helm is available on the local PATH.
+    IO.delete(targetDir)
+    IO.createDirectory(targetDir)
+
+    val packageCommand = Seq(
+      "helm",
+      "package",
+      tmpDir.getAbsolutePath(),
+      "--destination",
+      targetDir.getAbsolutePath()
+    )
+    val packageResult = Process(packageCommand).!
+    if (packageResult != 0) {
+      sys.error(s"`helm package` failed with exit code $packageResult")
+    }
+  }
+
+  // Assumes chart-releaser is available on the local PATH,
+  // and that CR_TOKEN is in the environment.
+  def cr(cmd: String, args: (String, String)*): Unit = {
+    val fullCommand =
+      "cr" :: cmd :: args.toList.flatMap { case (k, v) => List(k, v) }
+    val result = Process(fullCommand).!
+    if (result != 0) {
+      sys.error(s"`cr $cmd` failed with exit code $result")
+    }
+  }
+
   override def projectSettings: Seq[Def.Setting[_]] = Seq(
     helmStagingDirectory := target.value / "helm",
     helmChartLocalIndex := helmStagingDirectory.value / "index.yaml",
+    packageHelmChart / fileInputs += baseDirectory.value.toGlob / **,
     packageHelmChart := {
-      // Assumes the project is a valid Helm chart.
-      val sourceDir = baseDirectory.value
-      val tmpDir = IO.createTemporaryDirectory
-      IO.copyDirectory(sourceDir, tmpDir)
+      val inputChanges = packageHelmChart.inputFileChanges
+      val targetDir = target.value.toPath
+      val filteredChanges = List
+        .concat(
+          inputChanges.created,
+          inputChanges.modified,
+          inputChanges.deleted
+        )
+        .filterNot(_.startsWith(targetDir))
 
-      // Inject the version and appVersion so packaging does the right thing.
-      val chartMetadata = sourceDir / "Chart.yaml"
-      val parsedMetadata = yaml.parser.parse(IO.read(chartMetadata)) match {
-        case Right(parsed) => parsed
-        case Left(err)     => sys.error(s"Could not parse $chartMetadata as YAML: ${err.getMessage}")
+      val packageTarget = helmStagingDirectory.value
+      if (filteredChanges.nonEmpty) {
+        packageChart(baseDirectory.value, version.value, packageTarget)
       }
-      val realVersion = Json.fromString(version.value)
-      val updatedMetadata =
-        parsedMetadata.deepMerge(Json.obj("version" -> realVersion, "appVersion" -> realVersion))
-      IO.write(tmpDir / "Chart.yaml", updatedMetadata.asYaml.spaces2)
-
-      // Make sure the target directory isn't included in the chart package.
-      val ignores = sourceDir / ".helmignore"
-      val defaultIgnores = if (ignores.exists) IO.readLines(ignores) else Nil
-      val ignoresWithTarget = "target" :: defaultIgnores
-      IO.writeLines(tmpDir / ".helmignore", ignoresWithTarget)
-
-      // Use helm to package the staged template.
-      // Assumes helm is available on the local PATH.
-      val targetDir = helmStagingDirectory.value
-      IO.delete(targetDir)
-      IO.createDirectory(targetDir)
-
-      val packageCommand = Seq(
-        "helm",
-        "package",
-        tmpDir.getAbsolutePath(),
-        "--destination",
-        targetDir.getAbsolutePath()
-      )
-      val packageResult = Process(packageCommand).!
-      if (packageResult != 0) {
-        sys.error(s"`helm package` failed with exit code $packageResult")
-      }
-      targetDir
+      packageTarget
     },
     releaseHelmChart := {
       val log = streams.value.log
       val packageDir = packageHelmChart.value.getAbsolutePath()
       val org = helmChartOrganization.value
       val repo = helmChartRepository.value
-      val indexTarget = helmChartLocalIndex.value
-
-      // Assumes chart-releaser is available on the local PATH,
-      // and that CR_TOKEN is in the environment.
-      def cr(cmd: String, args: (String, String)*): Unit = {
-        val fullCommand =
-          "cr" :: cmd :: args.toList.flatMap { case (k, v) => List(k, v) }
-        val result = Process(fullCommand).!
-        if (result != 0) {
-          sys.error(s"`cr $cmd` failed with exit code $result")
-        }
-      }
 
       log.info(s"Uploading Helm chart for ${name.value} to $org/$repo...")
       cr("upload", "-o" -> org, "-r" -> repo, "-p" -> packageDir)
+    },
+    reindexHelmRepository := {
+      val log = streams.value.log
+      val packageDir = packageHelmChart.value.getAbsolutePath()
+      val org = helmChartOrganization.value
+      val repo = helmChartRepository.value
+      val indexTarget = helmChartLocalIndex.value
 
       val indexSite = s"https://$org.github.io/$repo"
       log.info(s"Adding Helm chart for ${name.value} to index for $indexSite...")
@@ -116,7 +138,7 @@ object MonsterHelmPlugin extends AutoPlugin {
       log.info(s"Pushing updated index to $indexSite...")
       val attemptPushingIndex = Try {
         git("checkout", "gh-pages")
-        IO.copy(List(indexTarget -> gitBase / "index.yaml"))
+        IO.copyFile(indexTarget, gitBase / "index.yaml")
         git("add", "index.yaml")
         git("commit", "-m", "Update index.")
         git("push", "-f", "origin", "gh-pages")
